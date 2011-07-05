0d26bd482668ad5f35069ed913f70ea687213b05
Kirill Klenov <horneds@gmail.com>
2011-07-05 23:09:57 +0400

    Add broadcast.

--- handler.py
+++ handler.py
@@ -1,7 +1,14 @@
 import tornado.web
+from connection import ClientConnection
 
 
 class BroadcastHandler(tornado.web.RequestHandler):
 
     def get(self):
         self.render('console.html')
+
+    def post(self):
+        message = self.get_argument('message')
+        for client in ClientConnection.clients:
+            client.send(message)
+        self.write('message send.')
